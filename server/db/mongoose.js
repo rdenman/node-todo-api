7627a582bba1b00aff06d0d9b00ae7627526344d
= <rdenman@Robs-MacBook-Air.local>
2018-12-15 15:30:40 -0600

    add DELETE /users/me/token route and tests; fix deprecation warnings on startup

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
 
+const config = {
+  autoIndex: false,
+  useNewUrlParser: true
+};
+
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, config);
 
 module.exports = { mongoose };
